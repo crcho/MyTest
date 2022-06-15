55db36483e781541357f69ecf2d2eee5a3c8b9e5
crcho <chunrae@naver.com>
2022-06-15 12:27:57 +0900

    test commit form staging to local repository

---
+++ ListDetailExample/app/src/main/java/com/sample/listdetailexample/ChartItem.java
@@ -0,0 +1,9 @@
+package com.sample.listdetailexample;
+
+public class ChartItem {
+    public int id;
+    public int rank;
+    public String title;
+    public String singer;
+    public String imageFile;
+}
---
+++ ListDetailExample/app/src/main/java/com/sample/listdetailexample/DetailItem.java
@@ -0,0 +1,10 @@
+package com.sample.listdetailexample;
+
+public class DetailItem {
+    public int id;
+    public String title;
+    public String singer;
+    public String melodizer;
+    public String lyricist;
+    public String genre;
+}
